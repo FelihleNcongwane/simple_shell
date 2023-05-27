b74c0dc47f728228c10f2490e6026c9121119aef
Felihle <ncongwanefelihle02@gmail.com>
2023-05-26 22:22:51 -0700

    utilities.c

--- utilities.c
+++ utilities.c
@@ -1,140 +1,63 @@
 #include "shell.h"
 
 /**
- * interactive - Returns true if the shell is in interactive mode.
- * @info: Struct address.
- *
- * Return: 1 if interactive mode, 0 otherwise.
+ * Return: 1 if interactive mode, 0 otherwise
  */
 int interactive(info_t *info)
-{
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+{return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
-/**
- * is_delim - Checks if a character is a delimiter.
- * @c: The character to check.
- * @delim: The delimiter string.
- * Return: 1 if true, 0 if false.
- */
 int is_delim(char c, char *delim)
 {
-	for (; *delim; delim++)
-	{
-		if (*delim == c)
+	while (*delim)
+		if (*delim++ == c)
 			return (1);
-	}
 	return (0);
 }
 
 /**
- * _isalpha - Checks for an alphabetic character.
- * @c: The character to check.
- * Return: 1 if c is alphabetic, 0 otherwise.
+ * Return: 1 if c is alphabetic, 0 otherwise
  */
+
 int _isalpha(int c)
 {
-	switch (c)
-	{
-		case 'a':
-		case 'A':
-		case 'b':
-		case 'B':
-		case 'c':
-		case 'C':
-		case 'd':
-		case 'D':
-		case 'e':
-		case 'E':
-		case 'f':
-		case 'F':
-		case 'g':
-		case 'G':
-		case 'h':
-		case 'H':
-		case 'i':
-		case 'I':
-		case 'j':
-		case 'J':
-		case 'k':
-		case 'K':
-		case 'l':
-		case 'L':
-		case 'm':
-		case 'M':
-		case 'n':
-		case 'N':
-		case 'o':
-		case 'O':
-		case 'p':
-		case 'P':
-		case 'q':
-		case 'Q':
-		case 'r':
-		case 'R':
-		case 's':
-		case 'S':
-		case 't':
-		case 'T':
-		case 'u':
-		case 'U':
-		case 'v':
-		case 'V':
-		case 'w':
-		case 'W':
-		case 'x':
-		case 'X':
-		case 'y':
-		case 'Y':
-		case 'z':
-		case 'Z':
-			return (1);
-		default:
-			return (0);
-	}
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
 
 /**
- * _atoi - Converts a string to an integer.
- * @s: The string to be converted.
- * Return: 0 if no numbers in the string, converted number otherwise.
+ * Return: 0 if no numbers in string, converted number otherwise
  */
+
 int _atoi(char *s)
 {
-	int sign = 1, output;
-	unsigned int result = 0;
+	int i;
+	int sign = 1;
+        int flag = 0;
+       	int output;
+	unsigned int results = 0;
 
-	for (int i = 0, flag = 0; s[i] != '\0' && flag != 2; i++)
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
 	{
-		switch (s[i])
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			case '-':
-				sign *= -1;
-				break;
-			case '0':
-			case '1':
-			case '2':
-			case '3':
-			case '4':
-			case '5':
-			case '6':
-			case '7':
-			case '8':
-			case '9':
-				flag = 1;
-				result *= 10;
-				result += (s[i] - '0');
-				break;
-			default:
-				if (flag == 1)
-					flag = 2;
+			flag = 1;
+			results *= 10;
+			results += (s[i] - '0');
 		}
+		else if (flag == 1)
+			flag = 2;
 	}
 
 	if (sign == -1)
-		output = -result;
+		output = -results;
 	else
-		output = result;
+		output = results;
 
 	return (output);
 }
