e7af2dac8e4f08765153a95edb3da7b18cb123d5
Felihle <ncongwanefelihle02@gmail.com>
2023-05-26 21:45:43 -0700

    io_functions.c

---
+++ io_functions.c
@@ -0,0 +1,87 @@
+/* io_functions.c */
+
+#include "shell.h"
+
+/**
+ * _eputs - Prints an input string.
+ * @str: The string to be printed.
+ *
+ * Return: Nothing.
+ */
+void _eputs(char *str)
+{
+	if (!str)
+		return;
+
+	for (int i = 0; str[i] != '\0'; i++)
+		_eputchar(str[i]);
+}
+
+/**
+ * _eputchar - Writes the character c to stderr.
+ * @c: The character to print.
+ *
+ * Return: On success 1.
+ *         On error, -1 is returned, and errno is set appropriately.
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+
+	return (1);
+}
+
+/**
+ * _putfd - Writes the character c to the given file descriptor.
+ * @c: The character to print.
+ * @fd: The file descriptor to write to.
+ *
+ * Return: On success 1.
+ *         On error, -1 is returned, and errno is set appropriately.
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+
+	return (1);
+}
+
+/**
+ * _putsfd - Prints an input string.
+ * @str: The string to be printed.
+ * @fd: The file descriptor to write to.
+ *
+ * Return: The number of characters put.
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+
+	for (; *str; str++)
+		i += _putfd(*str, fd);
+
+	return (i);
+}
